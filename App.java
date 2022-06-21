daebb59c10c56b8cc30705e5c3198a837ac96913
akash <akash300383@gmail.com>
2022-06-21 13:45:06 +0530

    done some changes

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 class App {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Simple Java program runnig in jenkin environment");
+	System.out.println("Hello, I done some changes in code");
 	}
 }
